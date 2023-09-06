8b6af797d0cebf13f265b83ee554417e8289c89d
phamtanphat <phatdroid94@gmail.com>
2023-09-06 19:01:54 +0700

    case re-create activity

--- app/src/main/java/com/example/mvvm19052023/MainActivity.kt
+++ app/src/main/java/com/example/mvvm19052023/MainActivity.kt
@@ -2,30 +2,25 @@ package com.example.mvvm19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var edtInput: EditText
+    private lateinit var tvInput: TextView
+    private lateinit var btnPrintInput: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var mutableLiveDataString = MutableLiveData<String>()
+        edtInput = findViewById(R.id.edit_text_input)
+        tvInput = findViewById(R.id.text_view_input)
+        btnPrintInput = findViewById(R.id.button_print_input)
 
-        // Read
-        mutableLiveDataString.observe(this@MainActivity) {
-            Log.d("BBB", it)
-        }
-
-        // Write
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
-            mutableLiveDataString.value = "New data"
+        btnPrintInput.setOnClickListener {
+            tvInput.text = edtInput.text.toString()
         }
     }
 }
